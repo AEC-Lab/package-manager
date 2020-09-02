302eb50b68b700c7d7dfac6f0cf3faeb988b7d3b
Grant Foster <grantdfoster@gmail.com>
2020-09-02 11:55:26 -0400

    initial push

--- src/App.vue
+++ src/App.vue
@@ -1,32 +1,78 @@
 <template>
-  <div id="app">
-    <div id="nav">
-      <router-link to="/">Home</router-link> |
-      <router-link to="/about">About</router-link>
-    </div>
-    <router-view />
-  </div>
+  <v-app>
+    <!-- <v-app-bar app color="primary" dark></v-app-bar> -->
+    <v-card :height="height">
+      <v-navigation-drawer
+        width="200"
+        v-model="drawer"
+        :mini-variant.sync="mini"
+        :expand-on-hover="true"
+        :permanent="true"
+        dark
+      >
+        <v-list dense nav class="py-0">
+          <v-list-item two-line :class="true && 'px-0'">
+            <v-list-item-avatar>
+              <v-avatar color="indigo" size="36">G</v-avatar>
+            </v-list-item-avatar>
+
+            <v-list-item-content>
+              <v-list-item-title>Grant</v-list-item-title>
+              <v-list-item-subtitle>AEC Lab</v-list-item-subtitle>
+            </v-list-item-content>
+          </v-list-item>
+
+          <v-divider></v-divider>
+
+          <v-list-item v-for="item in items" :key="item.title" link>
+            <v-list-item-icon>
+              <v-icon>{{ item.icon }}</v-icon>
+            </v-list-item-icon>
+            <v-list-item-content>
+              <v-list-item-title>{{ item.title }}</v-list-item-title>
+            </v-list-item-content>
+          </v-list-item>
+        </v-list>
+        <template v-slot:append>
+          <div v-if="!mini" class="pa-2">
+            <v-btn block>Logout</v-btn>
+          </div>
+        </template>
+      </v-navigation-drawer>
+    </v-card>
+    <!-- <div id="router">
+      <router-view></router-view>
+    </div> -->
+  </v-app>
 </template>
 
-<style lang="scss">
-#app {
-  font-family: Avenir, Helvetica, Arial, sans-serif;
-  -webkit-font-smoothing: antialiased;
-  -moz-osx-font-smoothing: grayscale;
-  text-align: center;
-  color: #2c3e50;
-}
+<script lang="ts">
+import Vue from "vue";
 
-#nav {
-  padding: 30px;
-
-  a {
-    font-weight: bold;
-    color: #2c3e50;
-
-    &.router-link-exact-active {
-      color: #42b983;
+// import { Component } from "vue-property-decorator";
+// @Component({
+//   components: {}
+// })
+export default Vue.extend({
+  name: "App",
+  data: () => ({
+    drawer: true,
+    mini: true,
+    items: [
+      { title: "Browse", icon: "mdi-archive" },
+      { title: "Settings", icon: "mdi-cog" },
+      { title: "Admin", icon: "mdi-shield" }
+    ]
+  }),
+  computed: {
+    height() {
+      return window.innerHeight;
     }
   }
+});
+</script>
+<style lang="scss" scoped>
+#router {
+  float: right;
 }
 </style>
