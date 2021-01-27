ce8681a51e339744d9d8bcbef7c217f7fde4aa84
Francisco Possetto <franciscopossetto@hotmail.com>
2021-01-27 11:06:15 -0300

    modify theme,add colors, add settings page

--- src/plugins/vuetify.ts
+++ src/plugins/vuetify.ts
@@ -2,5 +2,18 @@ import Vue from "vue";
 import Vuetify from "vuetify/lib";
 
 Vue.use(Vuetify);
-
+const vuetify = new Vuetify({
+  theme: {
+    themes: {
+      light: {
+        primary: "#493e96",
+        secondary: "#6c8088"
+      },
+      dark: {
+        primary: "#493e96",
+        secondary: "#f4f4f4"
+      }
+    }
+  }
+});
 export default new Vuetify({});
--- src/views/Settings.vue
+++ src/views/Settings.vue
@@ -1,29 +1,63 @@
 <template>
-  <v-container class="vo-container">Settings </v-container>
+  <v-container class="vo-container">
+    <v-expansion-panels id="container">
+      <v-expansion-panel>
+        <v-expansion-panel-header>
+          <span class="title font-weight-bold">Personal information</span>
+        </v-expansion-panel-header>
+        <v-expansion-panel-content>
+          <v-text-field label="Name" placeholder="Complete your name" outlined v-model="name"></v-text-field>
+          <p>Your name may appear around Package Manager when you install a package</p>
+          <br />
+
+          <v-text-field label="Email" placeholder="Set your Email" outlined v-model="email"></v-text-field>
+          <v-text-field
+            label="Password"
+            placeholder="Set your password"
+            type="password"
+            outlined
+            v-model="pass"
+          ></v-text-field>
+          <br />
+          <v-text-field
+            label="Company"
+            placeholder="Complete your name"
+            outlined
+            v-model="company"
+          ></v-text-field>
+          <v-text-field label="URL" placeholder="Complete your name" outlined v-model="url"></v-text-field>
+          <v-text-field
+            label="Location"
+            placeholder="Complete your name"
+            outlined
+            v-model="location"
+          ></v-text-field>
+
+          <v-btn class="float-right" color="primary">
+            Update profile
+          </v-btn>
+        </v-expansion-panel-content>
+      </v-expansion-panel>
+    </v-expansion-panels>
+  </v-container>
 </template>
 
 <script lang="ts">
-import { defineComponent, ref, computed } from "@vue/composition-api";
+import { Component, Vue } from "vue-property-decorator";
 
-export default defineComponent({
-  setup(props, context) {
-    const test = ref("test");
-    const $store = context.root.$store;
-
-    const repositories = computed(() => {
-      return $store.state.repositories;
-    });
-
-    return {
-      test,
-      repositories
-    };
-  }
-});
+@Component
+export default class Settings extends Vue {
+  name = "Fran";
+  email = "franp@voyansi.com";
+  pass = "123456";
+  company = "Voyansi";
+  url = "www.voyansi.com";
+  location = "US";
+}
 </script>
 <style lang="scss">
 #container {
   background-color: rgb(224, 224, 224);
-  height: 100vh;
+  width: 50%;
 }
 </style>
